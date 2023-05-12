7b89cc0162bbc08fc1fbd0a13c893a308d7b0df7
Tamás Szincsák <tamas@appcorner.eu>
2023-05-13 01:12:57 +0200

    Removed support for GitLab CI

--- update-ci.sh
+++ update-ci.sh
@@ -1,29 +1,3 @@
-# GitLab CI
-cat << EOF > .gitlab-ci.yml
-image: docker:latest
-
-before_script:
-  - docker login -u gitlab-ci-token -p \$CI_JOB_TOKEN \$CI_REGISTRY
-
-EOF
-
-for dir in *; do
-	if [ -d "$dir" ]; then
-		cat << EOF >> .gitlab-ci.yml
-build_$dir:
-  stage: build
-  only:
-    - master
-  script:
-    - cd $dir
-    - docker build --pull -t "\$CI_REGISTRY_IMAGE:$dir" .
-    - docker push "\$CI_REGISTRY_IMAGE:$dir"
-
-EOF
-	fi
-done
-
-# GitHub actions
 cat << EOF > .github/workflows/ci.yml
 name: Publish Docker images
 
